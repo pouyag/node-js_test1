1fd38cc9a675d03fe56dd9297018e07de6a403ed
pouyag <pouyagonbadi@yahoo.com>
2023-07-22 10:38:44 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var server=express()
     console.log('hello world')
-server.get('/',function(req,res){
+var http=require('http')
+http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type': 'text/html'})
     res.write('hello world')
     res.end()
-})
-})
+}).listen(8080);
 
 
 
